7bd680921c17178a40ecb08b04f33f358152c81f
Brian Gerkey <gerkey@osrfoundation.org>
2016-10-27 11:38:55 -0700

    first draft of automating build and package

---
+++ packaging/build_package.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+WS=`pwd`/ws
+INSTALL=/opt/sasc-foo
+REPOS=./gazebo_uctf.repos
+
+if test -d ${INSTALL}; then
+  echo "Error: Install directory ${INSTALL} exists. Please delete it and try again."
+  exit 1
+fi
+
+mkdir -p ${WS}/src
+
+echo "Cloning from ${REPOS} into ${WS}/src..."
+vcs import --input ${REPOS} ${WS}/src
+
+echo "Downloading pacakge.xml files for Gazebo pacakges..."
+curl https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_gazebo.xml > ${WS}/src/gazebo/package.xml
+curl https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_ign-math.xml > ${WS}/src/ign-math/package.xml
+curl https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_ign-msgs.xml > ${WS}/src/ign-msgs/package.xml
+curl https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_ign-tools.xml > ${WS}/src/ign-tools/package.xml
+curl https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_sdformat.xml > ${WS}/src/sdformat/package.xml
+
+echo "Installing dependencies with rosdep..."
+. /opt/ros/kinetic/setup.bash
+rosdep install --from-path ${WS}/src --ignore-src || true
+
+echo "Building Gazebo and friends..."
+cd ${WS}
+catkin config --init --extend /opt/ros/kinetic -i ${INSTALL} --install --isolate-devel
+sudo mkdir -p ${INSTALL}
+sudo chown -R ${USER}:${USER} ${INSTALL}
+catkin build
+
+echo "Cloning Ardupilot..."
+git clone https://github.com/tfoote/ardupilot.git -b uctf-dev
+echo "Building Ardupilot..."
+cd ardupilot
+export PATH=${PATH}:${WS}/ardupilot/Tools/autotest
+./Tools/scripts/install-prereqs-ubuntu.sh
+git submodule update --init --recursive
+./waf configure --prefix=${INSTALL}
+./waf
+./waf install
+
