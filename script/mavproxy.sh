8f52d5cf52d8bedd48a2f74677db7387d7622ebf
Pierre Kancir <pierre.kancir.emn@gmail.com>
2016-12-08 18:16:23 +0100

    Correct mavproxy param for RC input

--- script/mavproxy.sh
+++ script/mavproxy.sh
@@ -2,5 +2,5 @@
 
 arguments=$@
 
-echo mavproxy.py --mav10 --sitl 127.0.0.1:5501 $arguments
-mavproxy.py --mav10 --sitl 127.0.0.1:5501 $arguments
+echo mavproxy.py --mav10 $arguments
+mavproxy.py --mav10 $arguments
