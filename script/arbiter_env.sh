cd5facde2bcf64d84030be29157af3c57d18d6bb
Tully Foote <tfoote@osrfoundation.org>
2017-02-03 14:25:10 -0800

    simplify arbiter env script

--- script/arbiter_env.sh
+++ script/arbiter_env.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
+
+unset PYTHONPATH
 export INSTALL_SPACE=/opt/sasc
 . ${INSTALL_SPACE}/venv3/bin/activate
-. ${INSTALL_SPACE}/setup.sh
-log="arbiter_log"
-eval "$@" \&
-PID=$!
-echo "running" "$@" "in PID $PID"> $log
-{ (cat <&3 3<&- >/dev/null; kill $PID; echo "killed" >> $log) & } 3<&0
-trap "echo EXIT >> $log" EXIT
-wait $PID
+exec "$@"
