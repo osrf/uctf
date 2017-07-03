2fa6ded45027e89251475d8776f9f63d836ca2dd
Tully Foote <tfoote@osrfoundation.org>
2017-07-03 13:56:38 -0700

    use locally defined SCRIMMAGE_TACTIC_INTERFACE_FILE if already set

--- script/payload_env.sh
+++ script/payload_env.sh
@@ -6,6 +6,6 @@ export INSTALL_SPACE=/opt/sasc
 . ${INSTALL_SPACE}/share/uctf/setup.sh
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${INSTALL_SPACE}/share/gazebo_models
 export PYTHONPATH=/home/ubuntu/scrimmage-templates/plugins/autonomy/python:$PYTHONPATH
-export SCRIMMAGE_TACTIC_INTERFACE_FILE=/home/ubuntu/scrimmage-templates/plugins/autonomy/python/behaviors.xml
+export SCRIMMAGE_TACTIC_INTERFACE_FILE=${SCRIMMAGE_TACTIC_INTERFACE_FILE:=/home/ubuntu/scrimmage-templates/plugins/autonomy/python/behaviors.xml}
 
 exec "$@"
